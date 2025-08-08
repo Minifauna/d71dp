4f5590253a48df8d8abff1d4ad14555e55c9c607
Moose <alcesgigapytest@gmail.com>
2025-08-08 12:34:29 -0400

    clean up some formatting and remove unused imports

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
-from sqlalchemy import Integer, String, ForeignKey
+from flask_login import UserMixin, login_user, LoginManager
+from flask_login import current_user, logout_user, login_required
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy.exc import SQLAlchemyError
 from flask_bootstrap import Bootstrap5
 from werkzeug.security import generate_password_hash, check_password_hash
-from functools import wraps
-from typing import List
 import os
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 import pandas as pd
 import re
 from flaskforms import Login, AddTask, Register, RemoveTask
-from projects import translation
 
 load_dotenv()
 
@@ -22,11 +20,14 @@ app.config['SECRET_KEY'] = os.getenv('FLASK_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 Bootstrap5(app)
 
+
 class Base(DeclarativeBase):
     pass
 
+
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
+
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
@@ -35,6 +36,7 @@ class User(db.Model, UserMixin):
     email: Mapped[str] = mapped_column(String(250), unique=True)
     password_hash: Mapped[str] = mapped_column(String(250))
     todo = relationship("Todo", back_populates="user")
+
 
 class Todo(db.Model):
     __tablename__ = "todo"
@@ -49,13 +51,17 @@ with app.app_context():
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -64,7 +70,9 @@ def register():
         username = form.username.data
         email = form.email.data
         password_hash = generate_password_hash(form.password.data)
-        new_user = User(username=username, email=email, password_hash=password_hash)
+        new_user = User(username=username,
+                        email=email,
+                        password_hash=password_hash)
         try:
             db.session.add(new_user)
             flash('Thank you!')
@@ -74,13 +82,15 @@ def register():
             return render_template("login.html", form=form)
     return render_template("login.html", form=form)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = Login()
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
-        result = db.session.execute(db.select(User).where(User.username == username))
+        result = db.session.execute(db.select(User)
+                                    .where(User.username == username))
         user = result.scalar()
         if check_password_hash(user.password_hash, password):
             login_user(user)
@@ -89,27 +99,32 @@ def login():
             return render_template("login.html", form=form)
     return render_template("login.html", form=form)
 
+
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
 @app.route('/tropes')
 def horror_tropes():
     data = pd.read_json('static/assets/HorrorTropes.json', orient='index')
     df_tropes = data.sample(5)
-    trope_names = [' '.join(filter(None, re.split(r'(?=[A-Z])', name))) for name in df_tropes.index]
+    trope_names = [' '.join(filter(None, re.split(r'(?=[A-Z])', name)))
+                   for name in df_tropes.index]
     trope_urls = [value[0] for value in df_tropes.values]
     trope_zip = zip(trope_names, trope_urls)
     trope_dict = dict(trope_zip)
     num_tropes = len(trope_dict)
     return render_template("tropes.html", tropes=trope_dict, length=num_tropes)
 
+
 @app.route('/all-tropes')
 def all_tropes():
     df_tropes = pd.read_json('static/assets/HorrorTropes.json', orient='index')
-    trope_names = [' '.join(filter(None, re.split(r'(?=[A-Z])', name))) for name in df_tropes.index]
+    trope_names = [' '.join(filter(None, re.split(r'(?=[A-Z])', name)))
+                   for name in df_tropes.index]
     trope_urls = [value[0] for value in df_tropes.values]
     trope_zip = zip(trope_names, trope_urls)
     temp_dict = dict(trope_zip)
@@ -118,14 +133,17 @@ def all_tropes():
     num_tropes = len(trope_dict)
     return render_template("tropes.html", tropes=trope_dict, length=num_tropes)
 
+
 @app.route('/devtest')
 def devtest():
     return render_template('devtest.html')
 
+
 @app.route('/todo', methods=['GET', 'POST'])
 @login_required
 def todo_list():
-    result = db.session.execute(db.select(Todo).where(Todo.user == current_user))
+    result = db.session.execute(db.select(Todo)
+                                .where(Todo.user == current_user))
     todo_list = result.scalars()
     form = AddTask()
     task_done = RemoveTask()
@@ -143,7 +161,11 @@ def todo_list():
         db.session.delete(finished_task)
         db.session.commit()
         return redirect(url_for('todo_list'))
-    return render_template('todo.html', todo=todo_list, form=form, remove=task_done)
+    return render_template('todo.html',
+                           todo=todo_list,
+                           form=form,
+                           remove=task_done)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
